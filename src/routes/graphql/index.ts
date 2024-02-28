1f92b73099ee3775a61ae16dcc5ed05dc13b02b2
Kostiantyn Sakharov <hellraisercw@gmail.com>
2024-02-28 17:31:37 +0200

    init: setting project

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -3,17 +3,21 @@ import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
 import { graphql } from 'graphql';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
+  const { prisma } = fastify; // prisma from fastify
   fastify.route({
     url: '/',
     method: 'POST',
     schema: {
       ...createGqlResponseSchema,
       response: {
-        200: gqlResponseSchema,
+        // 200: gqlResponseSchema,
       },
     },
     async handler(req) {
-      return {};
+      // do stuff req.body
+      return await prisma.user.findMany();
+      // return {};
+      // for n + 1 problem use 'in' operator
     },
   });
 };
